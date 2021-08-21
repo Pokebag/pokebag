a734ef647fe308b68e07cf64c35e3f56a9d70558
Trezy <t@trezy.com>
2021-08-21 18:01:35 -0500

    feat(web): add checks for SEO title and description

--- packages/web/src/components/BaseLayout.js
+++ packages/web/src/components/BaseLayout.js
@@ -1,7 +1,22 @@
 // Local imports
-import { Footer } from 'components/Footer'
 import { NextSeo as NextSEO } from 'next-seo'
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
+
+
+
+
+
+// Local imports
+import { Footer } from 'components/Footer'
+
+
+
+
+
+// Constants
+const MAX_DESCRIPTION_LENGTH = 300
+const MAX_TITLE_LENGTH = 50
 
 
 
@@ -13,6 +28,25 @@ export function BaseLayout(props) {
 		description,
 		title,
 	} = props
+
+	useEffect(() => {
+		if (title && (title.length > MAX_TITLE_LENGTH)) {
+			console.warn(`Page titles should be fewer than 60 characters, preferably closer to 50. This page's title is ${title.length} characters.`)
+		}
+
+		if (description) {
+			if (description.length > MAX_DESCRIPTION_LENGTH) {
+				console.error(`Page description is too long! The description should be 50-300 characters long, but this page's description is ${description.length} characters.`)
+			}
+
+			if (description.indexOf('"') !== -1) {
+				console.error('Page descriptions shouldn\'t contain double quotes.')
+			}
+		}
+	}, [
+		description,
+		title,
+	])
 
 	return (
 		<>
