8f0c28464822cd3400671f7dfaff3bf92a61a9c5
Trezy <t@trezy.com>
2021-08-21 13:21:27 -0500

    style(api): fix constant naming style

--- packages/api/src/helpers/unite/calculateHeldItemStats.js
+++ packages/api/src/helpers/unite/calculateHeldItemStats.js
@@ -2,13 +2,13 @@ export function calculateHeldItemStats(item) {
 	const CALCULATED_STATS = {}
 
 	for (let index = 0; index < 30; index ++) {
-		const lvl = index + 1
+		const LVL = index + 1
 
-		CALCULATED_STATS[lvl] = {
-			lvl,
+		CALCULATED_STATS[LVL] = {
+			lvl: LVL,
 			value: Object.entries(item.stats)
 				.reduce((accumulator, [statID, statData]) => {
-					const COMPLETE_FORMULA = statData.formula.replace(/\{lvl\}/, (index + 1))
+					const COMPLETE_FORMULA = statData.formula.replace(/\{lvl\}/, LVL)
 
 					accumulator[statID] = parseFloat(eval(COMPLETE_FORMULA))
 
