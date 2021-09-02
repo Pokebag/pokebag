36a98e06bd6cd4bab5968b759564c4213b9e3430
Trezy <t@trezy.com>
2021-09-01 23:36:08 -0500

    fix(api): flatten Pokémon data

--- packages/api/src/routes/unite/pokemon.js
+++ packages/api/src/routes/unite/pokemon.js
@@ -1,8 +1,5 @@
 // Module imports
-import {
-	getDirectory,
-	getFile,
-} from '@pokebag/data-sdk'
+import { getPokemon } from '@pokebag/data-sdk'
 
 
 
@@ -20,22 +17,13 @@ export class PokemonRoute extends Route {
 
 	handler = async context => {
 		try {
-			const POKEMON_FILES = await getDirectory('pokemon', context.params.patchVersion)
-
-			const ALL_POKEMON = await Promise.all(POKEMON_FILES.map(async filename => {
-				const POKEMON = await getFile(`pokemon/${filename}`, context.params.patchVersion)
-
-				return {
-					id: filename.replace(/\.json$/, ''),
-					data: POKEMON,
-				}
-			}))
+			const POKEMON = await getPokemon({
+				includeSkills: true,
+				patch: context.params.patchVersion,
+			})
 
 			context.data = {
-				pokemon: ALL_POKEMON.reduce((accumulator, pokemon) => {
-					accumulator[pokemon.id] = pokemon
-					return accumulator
-				}, {}),
+				pokemon: POKEMON,
 			}
 		} catch(error) {
 			console.log(error)
