eef55ee3ef6cfbfe63d927667f2307be61d8bc72
Trezy <t@trezy.com>
2021-09-03 11:00:12 -0500

    feat(web/bugs): add bug links to LeftNav

--- packages/web/src/components/Unite/LeftNav.js
+++ packages/web/src/components/Unite/LeftNav.js
@@ -7,61 +7,97 @@ import Link from 'next/link'
 
 
 
+// Local imports
+import { useAuth } from 'contexts/AuthContext'
+
+
+
+
+
 export function LeftNav() {
 	const Router = useRouter()
+	const { isLoggedIn } = useAuth()
 
 	return (
-		<aside className="box left-nav menu">
-			{/* <p className="menu-label">
-				Pokémon
-			</p>
+		<aside className="left-nav">
+			<div className="box menu">
+				<p className="menu-label">
+					Utilities
+				</p>
 
-			<ul className="menu-list">
-				<li>
-					<Link href="/unite/pokemon">All Pokémon</Link>
-				</li>
-			</ul> */}
+				<ul className="menu-list">
+					<li>
+						<Link href="/unite/item-upgrade-calculator">
+							<a
+								className={classnames({
+									'is-active': Router.pathname === '/unite/item-upgrade-calculator',
+								})}>
+								Item Upgrade Calculator
+							</a>
+						</Link>
+					</li>
 
-			<p className="menu-label">
-				Utilities
-			</p>
+					<li>
+						<Link href="/unite/known-bugs">
+							<a
+								className={classnames({
+									'is-active': Router.pathname === '/unite/known-bugs',
+								})}>
+								Known Bugs
+							</a>
+						</Link>
+					</li>
 
-			<ul className="menu-list">
-				<li>
-					<Link href="/unite/item-upgrade-calculator">
-						<a
-							className={classnames({
-								'is-active': Router.pathname === '/unite/item-upgrade-calculator',
-							})}>
-							Item Upgrade Calculator
-						</a>
-					</Link>
-				</li>
-			</ul>
+					<li>
+						<Link href="/unite/report-a-bug">
+							<a
+								className={classnames({
+									'is-active': Router.pathname === '/unite/report-a-bug',
+								})}>
+								Report a Bug
+							</a>
+						</Link>
+					</li>
+				</ul>
 
-			<p className="menu-label">
-				Items
-			</p>
+				<p className="menu-label">
+					Items
+				</p>
 
-			<ul className="menu-list">
-				<li>
-					<Link href="/unite/held-items">
-						<a
-							className={classnames({
-								'is-active': Router.pathname.startsWith('/unite/held-items'),
-							})}>
-								Held Items
-						</a>
-					</Link>
-				</li>
+				<ul className="menu-list">
+					<li>
+						<Link href="/unite/held-items">
+							<a
+								className={classnames({
+									'is-active': Router.pathname.startsWith('/unite/held-items'),
+								})}>
+									Held Items
+							</a>
+						</Link>
+					</li>
+				</ul>
+			</div>
 
-				{/* <li>
-					<a>Battle Items</a>
-					<ul>
-						{Object.values(items).map(mapItems)}
+			{isLoggedIn && (
+				<div className="box menu">
+					<p className="menu-label">
+						Admin
+					</p>
+
+					<ul className="menu-list">
+						<li>
+							<Link href="/unite/bug-reports">
+								<a
+									className={classnames({
+										'is-active': Router.pathname === '/unite/bug-reports',
+									})}>
+									Bug Reports
+								</a>
+							</Link>
+						</li>
 					</ul>
-				</li> */}
-			</ul>
+				</div>
+			)}
 		</aside>
 	)
 }
